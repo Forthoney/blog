2ad27ef50168856aaffcd08149b56f2e56658364
FortHoney <castlehoneyjung@gmail.com>
2022-10-21 23:28:20 -0400

    update rubocop and apply single quote fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,23 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_22_025940) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.bigint "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.bigint 'article_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
   end
 
-  add_foreign_key "comments", "articles"
+  add_foreign_key 'comments', 'articles'
 end
